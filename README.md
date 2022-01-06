ddc55e0e3e94465f398b5002c6757784ccead46c
Mojito <vu3xk41997@gmail.com>
2022-01-06 01:27:57 +0100

    pixel panel done

--- README.md
+++ README.md
@@ -15,3 +15,4 @@ Project for JavaScript foundation.
 * document.getElementById
 * appendChild
 * add style
+* addEventListener
--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 let input = document.getElementById('myRange');
+// When browser loads
 genDivs(20);
+hoverChangeColor("black");
 
 input.addEventListener('change', function(e){
     input.value = e.target.value;
-    console.log(input.value);
     document.getElementById('grid').innerHTML = "";
     genDivs(input.value);
+    hoverChangeColor(getRandomColor());
 });
 
 function genDivs(num){
@@ -16,3 +18,40 @@ function genDivs(num){
     };
     document.getElementById('grid').style.gridTemplateColumns = "auto ".repeat(num);
 };
+
+function hoverChangeColor(color){
+    let cells = document.querySelectorAll('.cell');
+    cells.forEach(cell => cell.addEventListener('mouseover', function(){
+        cell.style.backgroundColor = color;
+    }))
+};
+
+// input - pickColor
+let pickColor = document.getElementById('pickColor');
+pickColor.addEventListener('input', function(e){
+    hoverChangeColor(e.target.value);
+});
+
+// buttons - rainbow
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// buttons - eraser
+let eraser = document.getElementById('eraser');
+eraser.addEventListener('click',function(){
+    hoverChangeColor("white");
+})
+
+// buttons - clear
+let clear = document.getElementById('clear');
+clear.addEventListener('click',function(){
+    document.getElementById('grid').innerHTML = "";
+    genDivs(input.value);
+    hoverChangeColor(pickColor.value);
+})
